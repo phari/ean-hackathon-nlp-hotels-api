7ee992b5c0cfb17d957f68bcd2a7be72e4febbcb
Luke Mayes <lmayes@expedia.com>
2016-06-29 14:51:47 +0100

    Content search by property_id or city name

--- src/main/groovy/com/ean/hackathon/content/PropertyCache.groovy
+++ src/main/groovy/com/ean/hackathon/content/PropertyCache.groovy
@@ -1,15 +1,48 @@
 package com.ean.hackathon.content
 
-/**
- * Created by phari on 6/29/16.
- */
-class PropertyCache {
+import java.util.regex.Pattern
 
-    public static List<String> getHotels(String city, String country) {
-        if (city.toUpperCase().equals("NEW YORK")) {
-            return [ "19248" ]
+public class PropertyCache {
+    private static final Pattern PROPERTY_ID_PATTERN = Pattern.compile("\\{\"property_id\":\"(\\d+)\",\"name\":");
+    private static PropertyCache INSTANCE;
+    private Map<Integer, String> properties = [:]
+
+    public static PropertyCache instance() {
+        if (INSTANCE != null) {
+            return INSTANCE;
+        } else {
+            return INSTANCE = new PropertyCache();
         }
 
-        return []
     }
+
+    private PropertyCache() {
+        properties = PropertyCache.class.getClassLoader().getResourceAsStream("propertycatalog.jsonl").readLines()
+                .collectEntries {
+            [propertyId(it), it]
+        }
+    }
+
+    public String findById(String propertyId) {
+        return properties.getOrDefault(Integer.parseInt(propertyId), "property not found");
+    }
+
+    public List<String> findByCityName(String cityName) {
+        properties.findAll {
+            it.value.matches(/.*(?i)${cityName}.*/)
+        }.collect {
+            it.value
+        }
+    }
+
+    private static Integer propertyId(String line) {
+        def matcher = PROPERTY_ID_PATTERN.matcher(line)
+        return Integer.parseInt(matcher[0][1]);
+    }
+
+    // demo
+    public static void main(String[] args) {
+        println PropertyCache.instance().findByCityName("new yorK")
+    }
+
 }
