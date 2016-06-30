b6a6e3ca30e5c6dd85df10f9a389517d873388a8
Luke Mayes <lmayes@expedia.com>
2016-06-30 15:07:30 +0100

    More smarts on date ranges

--- src/main/groovy/com/ean/hackathon/nlp/NLPService.groovy
+++ src/main/groovy/com/ean/hackathon/nlp/NLPService.groovy
@@ -16,7 +16,7 @@ class NLPService {
         def result = [:]
         Map jsonEntities = client.getNLPEntities(text)
         if (jsonEntities.containsKey("datetime")) {
-            result.putAll(processDates(jsonEntities.datetime))
+            result.putAll(processDates(jsonEntities.datetime, jsonEntities.duration))
         }
         if (jsonEntities.containsKey("location")) {
             result.putAll(processLocations(jsonEntities.location))
@@ -35,42 +35,62 @@ class NLPService {
         locations ? [location: locations.max { it.confidence }.value] : [:]
     }
 
-    Map processDates(List dates) {
+    Map processDates(List dates, List durations) {
+        def result = [:]
         if (!dates.isEmpty()) {
             def intervals = dates.findAll { it.type == "interval" }
             if (!intervals.isEmpty()) {
                 def highestConfidence = intervals.max { it.confidence }
-                return [
+                result = [
                         from: FORMAT.parse(highestConfidence.from.value),
-                        to: FORMAT.parse(highestConfidence.to.value)
+                        to  : FORMAT.parse(highestConfidence.to.value) - 1
+                ]
+            } else if (durations) {
+                def from = FORMAT.parse(dates[0].value)
+                def duration = durations.max { it.confidence }?.value ?: 7
+                result = [
+                    from: from,
+                    to: from + (duration - 1)
                 ]
             } else {
                 def dateValues = dates.collect { FORMAT.parse(it.value) }
                 def min = dateValues.min()
                 def max = dateValues.max()
                 if (min == max) {
-                    // look for a duration
-
-                    return [
+                    result = [
                         from: min,
-                        to: new Date().plus(7)
+                        to: new Date().plus(6)
+                    ]
+                } else {
+                    result = [
+                        from: min,
+                        to  : max
                     ]
                 }
-                return [
-                    from: min,
-                    to: max
-                ]
             }
         }
 
-        [:]
+        def now = new Date()
+        if (now - result.from > 7) {
+            return [to:  result.to + 7, from:  result.from + 14]
+        } else if (now - result.from > 0) {
+            return [to:  result.to + 7, from:  result.from + 7]
+        }
+
+        result
     }
 
     public static void main(String[] args) {
+
+        "%Hilton Garden Inn%"
+
         println new NLPService().processRawText(
-            "I'd like to stay in London from next Tuesday to next Sunday. I'll be bringing my wife with me."
+                "Hotels in London from the tenth of July to the fifteenth of July."
         )
         // I'm looking for a hotel in New York next week. I'd like to stay for three days.
+        // Hotels in London from the tenth of July to the fifteenth of July.
+        // I'm looking for a hotel in New York next week. I'd like to stay for three days.
+        // I'd like to stay in London next week. I need something from next Tuesday to next Sunday.
 
         // Show me just the Hilton.
 
